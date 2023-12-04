d465e3712faee035e4e2666223c6f85bef31af8a
Caden Christesen <cadenchristesen@gmail.com>
2023-12-04 09:52:13 -0600

    Initial Commit

---
+++ GoingToTown/GoingToTownApp.swift
@@ -0,0 +1,17 @@
+//
+//  GoingToTownApp.swift
+//  GoingToTown
+//
+//  Created by Caden Christesen on 12/4/23.
+//
+
+import SwiftUI
+
+@main
+struct GoingToTownApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
